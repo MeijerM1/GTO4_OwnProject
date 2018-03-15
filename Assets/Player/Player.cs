690ac6549863cbb0dfe8020f5463689313d73d4b
MeijerM1 <max1_meijer@hotmail.com>
2018-03-15 17:27:54 +0100

    Add stuff

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string PlayerName;
+	public Color Color;
+
+	// Use this for initialization
+	void Awake ()
+	{
+        PlayerName = gameObject.name;
+		Color = Random.ColorHSV();
+	}	
+}
---
+++ Assets/Resources/Resource.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Resource : MonoBehaviour {
+
+    public int Amount;
+    public int StartingAmount;
+
+    public UnityEvent OnValueChange;
+
+    public void Awake()
+    {
+        Amount = StartingAmount;
+    }
+
+    public void AddAmount(int amount)
+    {
+        Amount += amount;
+        OnValueChange.Invoke();
+    }
+
+    public bool CanAfford(int costs)
+    {
+        return costs <= Amount;
+    }
+
+    public void RemoveAmount(int amount)
+    {
+        Amount -= amount;
+        OnValueChange.Invoke();
+    }
+}
